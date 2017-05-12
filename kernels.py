457458d862f3fc97cb436d0fe8139c4f960c78a8
Christoph Heindl <christoph.heindl@gmail.com>
2017-05-12 19:46:54 +0200

    using squared instead of standardised distance
    single character..

--- kernels.py
+++ kernels.py
@@ -3,6 +3,6 @@ from scipy.spatial.distance import cdist
 
 def se_kernel(x, y, sigma=1., length=1.):
     """Squared exponential kernel."""
-    d = cdist(x.reshape(-1,1), y.reshape(-1,1), metric='seuclidean')
+    d = cdist(x.reshape(-1,1), y.reshape(-1,1), metric='sqeuclidean')
     f = -1. / (2. * length**2)
     return sigma**2 * np.exp(d*f).reshape(x.shape[0], y.shape[0])
