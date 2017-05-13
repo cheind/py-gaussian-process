3c85a90b6bcda35605edb5b6f7bf4ddfa668dfff
Christoph Heindl <christoph.heindl@gmail.com>
2017-05-13 15:38:14 +0200

    cleanup

--- posterior.py
+++ posterior.py
@@ -3,49 +3,49 @@ import matplotlib.pyplot as plt
 from scipy.linalg import solve_triangular as solvetri
 
 import kernels
-import utils
 
 if __name__ == '__main__':
 
     np.random.seed(123)
 
+    # Parameters
+    ntrain = 4
+    klength = 1.0
+    ksigma = 2.    
+    noise = 0.2
+
     # True function
-    f = lambda x: np.polyval([0.5, -0.2, 0.2, 5.], x)
+    f = lambda x: np.polyval([0.1, -0.2, 0.1, 2.], x)
 
-    #f = lambda x: np.sin(0.9*x).flatten()
+    # Train points
+    xtrain = np.random.uniform(-4, 4, ntrain)
+    ytrain = f(xtrain) + np.random.randn(xtrain.shape[0]) * 0.2**2
 
-    # Train points / no noise
-    xtrain = np.array([-2, -1.8, 0.5, 1.5])
-    #xtrain = np.random.uniform(-5, 5, size=10)
-    ytrain = f(xtrain)
-
-    # Test points
+    # Test points we'd like to predict at
     xtest = np.linspace(-5., 5, 50, endpoint=True)
 
-    length = 0.5
-    sigma = 1.
+    # Blocks of the covariance matrix required
+    K = kernels.se_kernel(xtrain, xtrain, ksigma, klength) + np.eye(xtrain.shape[0]) * noise**2
+    Ks = kernels.se_kernel(xtrain, xtest, ksigma, klength)
+    Kss = kernels.se_kernel(xtest, xtest, ksigma, klength)
 
-    K = kernels.se_kernel(xtrain, xtrain, sigma, length)
-    Ks = kernels.se_kernel(xtrain, xtest, sigma, length)
-
-    # Posterior mean
+    # Posterior mean and covariance
     L = np.linalg.cholesky(K)
     alpha = solvetri(L.T, solvetri(L, ytrain, check_finite=False, lower=True), check_finite=False)
     pmu = Ks.T.dot(alpha)
-
-    # Posterior covariance
-    Kss = kernels.se_kernel(xtest, xtest, sigma, length)
     beta = solvetri(L, Ks, lower=True)
     pcov = Kss - beta.T.dot(beta)
 
-    xdraw = np.linspace(-5, 5, 100, endpoint=True)
-    ydraw = np.random.multivariate_normal(pmu, pcov, size=10)
+    # Draw samples (functions) from the posterior
+    ytest = np.random.multivariate_normal(pmu, pcov, size=10)
+
+    xdraw = np.linspace(-5, 5, 50, endpoint=True)
 
     fig, ax = plt.subplots()
-    ax.set_title('Ten samples from a GP posterior')
-    ax.plot(xdraw.reshape(-1, 1), utils.interp(xtest, ydraw.T, xdraw))
-    ax.plot(xtrain, ytrain, 'k+', ms=20, lw=5)
-    ax.plot(xtest, pmu, 'r--')
-    #plt.axis([-5, 5, -3, 3])
-    plt.tight_layout()
+    ax.set_title('Gaussian process posterior\nklength={:.1f}, ksigma={:.1f}, noise={:.1f}'.format(klength, ksigma, noise))    
+    ax.plot(xtest.reshape(-1, 1), ytest.T, lw=0.5)
+    ax.plot(xtrain, ytrain, 'k+', ms=20, lw=5, label='Train points')
+    ax.plot(xdraw, f(xdraw), 'k-', label='True function')
+    ax.plot(xtest, pmu, 'r--', label='Posterior mean')
+    plt.legend(loc=4)    
     plt.show()
